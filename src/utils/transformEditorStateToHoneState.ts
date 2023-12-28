50f5965bafb5bb5788185fad0f6278246294f727
bigU <joukee@gmail.com>
2023-12-28 15:37:20 +0800

    descriptive programming refers to: describe the functionality by nature language as skeleton, then add helper functions as muscle; distinguish the manner for condition if() and null check(set in helper fn or other manner) to make the two if obervious; the current bug is on the last block

--- src/utils/transformEditorStateToHoneState.ts
+++ src/utils/transformEditorStateToHoneState.ts
@@ -1,6 +1,14 @@
 import { RawDraftContentState } from "draft-js";
 import { Article, Facet, ArticleFacetLink, HoneState } from "../types/types";
 import { FACET_TITLE_SYMBOL } from "./constants";
+import { is } from "immutable";
+
+/* facet id is consisted of article id and block id
+so, preserve block id in db and convert it back to editorState when loading
+to leverage the persist nature of block id of draft-js while editing
+and the article as the editor merge other facet in is the article id to consist the facet id
+that is, editor is the source of truth of the facet id
+and destruct the facet id to article id and block id when loading to use block id back in editor */
 
 let article: Article = {} as Article;
 const articles: Record<string, Article> = {};
@@ -8,78 +16,90 @@ let currentFacet: Facet | null = null;
 const facets: Record<string, Facet> = {};
 const articleFacetLinks: ArticleFacetLink[] = [];
 
+// Helper functions to append facet and link
+function appendFacetAndLink(
+  currentFacet: Facet | null,
+  articleId: string,
+  index: number,
+  facets: Record<string, Facet>,
+  articleFacetLinks: ArticleFacetLink[]
+) {
+  if (currentFacet) {
+    if (currentFacet && currentFacet.facetId) {
+      facets[currentFacet.facetId] = currentFacet;
+
+      // push only if the link does not exist
+      const existingLinkIndex = articleFacetLinks.findIndex(
+        (link) => link.articleId === articleId && link.facetId === currentFacet.facetId
+      );
+
+      if (existingLinkIndex === -1) {
+        articleFacetLinks.push({
+          articleId,
+          facetId: currentFacet.facetId,
+          orderIndex: index,
+        });
+      }
+    }
+    console.log("facets1:", facets);
+    // Return updated state and reset current facet
+    return { facets, articleFacetLinks, currentFacet: null };
+  }
+}
+// Helper function to append text
+function appendText(currentText: string | undefined, newText: string) {
+  return currentText ? currentText + "\n" + newText : newText;
+}
+
 // Assemble article, facet, and articleFacetLink to be the honeState
-// facet id is consisted of article id and block id
-// so, preserve block id in db and convert it back to editorState when loading
-// to leverage the persist nature of block id of draft-js while editing
-// and the article as the editor merge other facet in is the article id to consist the facet id
-// that is, editor is the source of truth of the facet id
-// and destruct the facet id to article id and block id when loading to use block id back in editor
 export const transformEditorStateToHoneState = (
   articleId: string,
   articleDate: string,
   rawContentState: RawDraftContentState
 ): HoneState => {
   article = { articleId, date: articleDate };
+  let isFacetInitialized = false;
   rawContentState.blocks.forEach((block, index, array) => {
-    const blockId = block.key;
+    const isFirstBlock = index === 0;
+    const isFacetTitle = block.text.startsWith(FACET_TITLE_SYMBOL);
     const isLastBlock = index === array.length - 1;
+    const blockId = block.key;
 
-    if (index === 0) {
+    console.log("index:", index);
+    if (isFirstBlock) {
       article.title = block.text;
       articles[article.articleId] = article;
-    } else if (block.text.startsWith(FACET_TITLE_SYMBOL)) {
-      if (currentFacet) {
-        // Close the last facet while encountering a new facet title
-        facets[currentFacet.facetId!] = currentFacet;
-        const existingLinkIndex = articleFacetLinks.findIndex(
-          (link) => link.articleId === articleId && link.facetId === currentFacet!.facetId
-        );
-        // push only if the link does not exist
-        if (existingLinkIndex === -1) {
-          console.log("index in Links", index);
-          articleFacetLinks.push({
-            articleId,
-            facetId: currentFacet.facetId!,
-            orderIndex: index,
-          });
-        }
-
-        currentFacet = null; // reset current facet
+      console.log("article title:", article.title);
+    } else if (isLastBlock) {
+      console.log("last block");
+      if (isFacetInitialized) {
+        appendFacetAndLink(currentFacet, articleId, index, facets, articleFacetLinks);
+        isFacetInitialized = false;
+        console.log("last block as facet:", block.text);
+        console.log("facets2:", facets);
       } else {
-        currentFacet = {} as Facet;
-        currentFacet.facetId = `${articleId}-${blockId}`; // extract block id from facet id when loading
-        currentFacet.title = block.text;
+        article.nonFacet = appendText(article.nonFacet, block.text);
+        console.log("last block as non-facet:", block.text);
       }
+    } else if (isFacetTitle) {
+      if (isFacetInitialized) {
+        appendFacetAndLink(currentFacet, articleId, index, facets, articleFacetLinks);
+        isFacetInitialized = false;
+        console.log("append facet and link:", currentFacet);
+      }
+      // initialize a new facet
+      currentFacet = {} as Facet;
+      isFacetInitialized = true;
+      currentFacet.facetId = `${articleId}-${blockId}`;
+      currentFacet.title = block.text;
+      console.log("initialize a new facet:", block.text);
     } else {
-      if (currentFacet) {
-        currentFacet.content = (currentFacet.content ? currentFacet.content + "\n" : "") + block.text;
+      if (isFacetInitialized) {
+        currentFacet && (currentFacet.content = appendText(currentFacet?.content, block.text));
+        console.log("append facet content:", block.text);
       } else {
-        article.nonFacet = (article.nonFacet ? article.nonFacet + "\n" : "") + block.text;
-      }
-    }
-    if (isLastBlock) {
-      console.log("isLastBlock", isLastBlock);
-      console.log("currentFacet", currentFacet);
-      if (currentFacet) {
-        // Close the last facet while encountering a new facet title
-        facets[currentFacet.facetId!] = currentFacet;
-        const existingLinkIndex = articleFacetLinks.findIndex(
-          (link) => link.articleId === articleId && link.facetId === currentFacet!.facetId
-        );
-        // push only if the link does not exist
-        if (existingLinkIndex === -1) {
-          console.log("index in Links", index);
-          articleFacetLinks.push({
-            articleId,
-            facetId: currentFacet.facetId!,
-            orderIndex: index,
-          });
-        }
-
-        currentFacet = null; // reset current facet
-      } else {
-        article.nonFacet = (article.nonFacet ? article.nonFacet + "\n" : "") + block.text;
+        article.nonFacet = appendText(article.nonFacet, block.text);
+        console.log("append non-facet content:", block.text);
       }
     }
   });
