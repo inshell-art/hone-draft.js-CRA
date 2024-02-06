dc966d8d79bf9fe63bf2f4a21b44aa4f24570a73
Inshell <this.inshell@gmail.com>
2024-02-06 17:15:45 +0800

    sort CRA default files

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,19 +1,9 @@
-import { useNavigate } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { fetchAllArticles } from "../services/indexedDBService";
 import { Article } from "../types/types";
 
 const Articles = () => {
-  const navigate = useNavigate();
-
-  const createNewArticle = () => {
-    const newArticleId = uuidv4();
-    navigate(`/article/${newArticleId}`); // Transaction to save will be handled in Article.tsx to avoid empty articles creation
-  };
-
-  // fetch all articles
   const [articles, setArticles] = useState<Article[]>([]);
   useEffect(() => {
     fetchAllArticles().then((articles) => {
